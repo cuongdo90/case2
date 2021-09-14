1ed72b8a14db07a9ddc9d18af4e73e998c5608b3
cuongdo90 <cuongcoi9001th@gmail.com>
2021-09-14 12:07:38 +0700

    ok

---
+++ website_mvc/admin/inc/footer.php
@@ -0,0 +1,12 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="http://webextrasite.com">Đức Cường</a>. @ 2021.
+        </p>
+    </div>
+</body>
+</html>
